3faf3326ea92003ef9b6245b457c51a8cc78d773
Ondicho <ondicho53@gmail.com>
2020-09-21 08:01:32 +0300

    this commit adds pojo structure to endagered animal object

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Animal {
     private int id;
     private String name;
@@ -25,6 +27,22 @@ public class Animal {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Animal animal = (Animal) o;
+
+        if (id != animal.id) return false;
+        return Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return  Objects.hash(name);
     }
 }
 
--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,5 +1,54 @@
 package models;
 
+import java.util.Objects;
+
 public class EndangeredAnimal extends Animal {
 
+    private String health;
+    private String age;
+
+    //constants
+    public static final String MAX_HEALTH_LEVEL="healthy";
+    public static final String MID_HEALTH_LEVEL="okay";
+    public static final String MIN_HEALTH_LEVEL="ill";
+    public static final String MAX_AGE_LEVEL="adult";
+    public static final String MID_AGE_LEVEL="young";
+    public static final String MIN_AGE_LEVEL="newborn";
+
+    public EndangeredAnimal(String name, int id,String health,String age) {
+        super(name, id);
+        this.health=health;
+        this.age=age;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        EndangeredAnimal that = (EndangeredAnimal) o;
+        return Objects.equals(health, that.health) &&
+                Objects.equals(age, that.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), health, age);
+    }
 }
