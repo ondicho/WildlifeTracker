8eb763bb19305093d6adbd22d7d0d231bc0b6bc1
Ondicho <ondicho53@gmail.com>
2020-09-21 08:44:48 +0300

    adds sql commands to create.sql

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+    }
 }
---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE wildlife_tracker;
+CREATE TABLE animals (id serial PRIMARY KEY, name varchar);
+CREATE TABLE   sightings(id serial PRIMARY KEY,animalName varchar,animalAge int,animalHealth varchar,location varchar);
+CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
